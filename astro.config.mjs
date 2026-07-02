330ad0269a508f74a9ece99489d193b82d6b79ae
Tilen Tratnjek <80243427+Tilchy@users.noreply.github.com>
2026-07-02 11:17:32 +0200

    feat: add sitemap integration and update site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-  site: 'https://dualaiteacher.github.io',
+  site: 'https://dualaiteacher.eu',
+
   i18n: {
     defaultLocale: 'en',
     locales: ['cs', 'de', 'en', 'es', 'gb', 'lv', 'pl', 'sk', 'sl'],
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
